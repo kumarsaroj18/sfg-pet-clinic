b174bfc7a898d5d8135fe2b1cf173e7404919754
Saroj Kumar <kumarsaroj18@gmail.com>
2018-11-11 17:21:40 +0530

    added multi-module functionality

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/models/PetType.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic.models;
+
+public class PetType extends BaseEntity{
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.models.Pet;
+
+public interface PetService extends CrudService<Pet, Long>{
+}
